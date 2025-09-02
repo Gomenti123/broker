ded390f1ef0964eb5fa932082315a794399b705b
mac <mac@macs-MacBook-Pro.local>
2025-09-02 22:26:06 +0100

    footer

--- app/comp/footer.tsx
+++ app/comp/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className='w-full grid  grid-cols-1 md:grid-cols-5 gap-[20px]'>
         <div className='col-span-1'>
             <h1 className='text-[30px] text-neutral-500'>Logo</h1>
-            <p className='text-neutral-400 text-[15px]' >The world's leading cryptocurrency exchange platform, trusted by millions of traders worldwide.</p>
+            <p className='text-neutral-400 text-[15px]' >The worlds leading cryptocurrency exchange platform, trusted by millions of traders worldwide.</p>
         </div>
         <div className='flex flex-col md:flex-row gap-[30px] justify-between col-span-4 w-[80%]' >
             <div className='flex flex-col gap-2'>
