6e16d4e8f97e7dbcdad60ddee59460293888508a
mac <mac@macs-MacBook-Pro.local>
2025-10-11 17:23:36 +0100

    update

--- app/comp/faq.tsx
+++ app/comp/faq.tsx
@@ -52,14 +52,14 @@ some questions for clarification. If the few answers we provided didn&apos;t ans
          <div className='bg-blue-900 h-[60px] rounded-md text-white flex justify-between items-center  px-[15px] py-[10px]'>
         <div className='flex items-center gap-1 text-[20px]'>
             <FaQuestionCircle className='text-amber-600' />
-             <p>What is Bitcoin ? </p>
+             <p>What is Bitcoin Mining ? </p>
         </div>
        
         <IoIosArrowDown onClick={()=>{setShow2(!show2), setShow1(false), setShow3(false)}} />
     </div>
     {show2 ?  <div className=' transition-all  duration-300 px-3 py-1 mt-[-9px] bg-blue-900 rounded-md text-white'>
         <div className='h-[2px] mb-1 bg-amber-700 outline-dotted'></div>
-        <p className='pt-1'>Bitcoin is a form of digital currency which is based on an open source code that was created and is held electronically. Bitcoin is a decentralized form of currency, meaning that it does not belong to any form of government and is not controlled by anyone.</p>
+        <p className='pt-1'>Bitcoin mining is analogous to the mining of gold, but its digital form. The process involves specialized computers solving algorithmic equations or hash functions. These problems help miners to confirm blocks of transactions held within the network. Bitcoin mining provides a reward for miners by paying out in Bitcoin in turn the miners confirm transactions on the blockchain. Miners introduce new Bitcoin into the network and also secure the system with transaction confirmation. They are also rewarded network fees for when they harvest new coin and a time when the last bitcoin is found mining will continue.</p>
     </div>
      : <div className='transition-all  duration-300'></div> }
    
@@ -68,14 +68,14 @@ some questions for clarification. If the few answers we provided didn&apos;t ans
          <div className='bg-blue-900 h-[60px] rounded-md text-white flex justify-between items-center px-[15px] py-[10px]'>
         <div className='flex items-center gap-1 text-[20px]'>
             <FaQuestionCircle className='text-amber-600' />
-             <p>What is Bitcoin ? </p>
+             <p>How Can I Sell Bitcoins ? </p>
         </div>
        
         <IoIosArrowDown onClick={()=>{setShow3(!show3), setShow1(false), setShow2(false)}} />
     </div>
     {show3 ?  <div className=' transition-all  duration-300 px-3 py-1 mt-[-9px] bg-blue-900 rounded-md text-white'>
         <div className='h-[2px] mb-1 bg-amber-700 outline-dotted'></div>
-        <p className='pt-1'>Bitcoin is a form of digital currency which is based on an open source code that was created and is held electronically. Bitcoin is a decentralized form of currency, meaning that it does not belong to any form of government and is not controlled by anyone.</p>
+        <p className='pt-1'>Bitcoins can be sold locally using LocalBitcoins, on Bitcoin brokerages / exchanges, using two-way Bitcoin Teller Machines (BTM’s) or you can pay for a good or service with them. Bitcoins can be sold to just about anyone as long as they have a Bitcoin address, and can be sold for any fiat currency in the world or traded for a physical good. Feel free to check out our recommended list of exchanges and brokerage services to sell your bitcoins online.</p>
     </div>
      : <div className='transition-all  duration-300'></div> }
    
