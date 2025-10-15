474a5e1436a173a92d2b8d8142437e95745f870e
mac <mac@macs-MacBook-Pro.local>
2025-10-15 13:13:36 +0100

    update

--- app/api/verify/[userID]/route.ts
+++ app/api/verify/[userID]/route.ts
@@ -3,46 +3,55 @@ import myUserModel from "@/app/utils/Model/userModel";
 import { Request } from "express";
 import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async (
-  req: NextRequest,
-  res: NextResponse,
-
-  { params }: any
-) => {
+export const POST = async (req: NextRequest, { params }: any) => {
   try {
     await dbConfig();
     const { userID } = await params;
-    const { token } = await req.json();
+    const token = await req.json();
+    // console.log(userID);
+    // console.log(token);
+
     const user = await myUserModel.findById(userID);
+    // console.log(getD.token);
+
     if (user) {
-      const tokenCheck = user.token === token;
-      if (tokenCheck) {
-        const getD = await myUserModel.findByIdAndUpdate(
-          userID,
-          { verify: true, token: "" },
-          { new: true }
-        );
-        return NextResponse.json({
-          message: "User Verified",
-          status: 200,
-          data: getD,
-        });
+      const checkIfVerified = user.verify === true;
+      if (!checkIfVerified) {
+        const tokenCheck = token.token === user.token;
+        if (tokenCheck) {
+          const getD = await myUserModel.findByIdAndUpdate(
+            userID,
+            { verify: true, token: "" },
+            { new: true }
+          );
+          return NextResponse.json({
+            message: "User Verified",
+            status: 200,
+            data: getD,
+          });
+        } else {
+          return NextResponse.json({
+            message: "Incorrect token",
+            status: 400,
+          });
+        }
       } else {
         return NextResponse.json({
-          message: "Incorrect Token",
+          message: "Account already verified",
           status: 400,
         });
       }
     } else {
       return NextResponse.json({
-        message: "User Not Found",
+        message: "User not found",
         status: 400,
       });
     }
-  } catch (error) {
+  } catch (error: any) {
     return NextResponse.json({
       message: "Error Occured",
       status: 400,
+      error: error.message,
     });
   }
 };
