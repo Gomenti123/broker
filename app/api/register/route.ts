9474d7e4fe9004925ff68bc4758a0c87eb2bc25f
mac <mac@macs-MacBook-Pro.local>
2025-10-18 00:34:31 +0100

    update

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,36 +4,38 @@ import bcrypt from "bcrypt";
 import myUserModel from "@/app/utils/Model/userModel";
 import { sendEmail } from "@/app/utils/email";
 
-export const POST = async (req: NextRequest, res: NextResponse) => {
-  // try {
-  //   await dbConfig();
-  //   const { name, email, password, phoneNumber } = await req.json();
-  //   const salt = await bcrypt.genSalt(10);
-  //   const hashed = await bcrypt.hash(password, salt);
-  //   const token = Math.floor(100000 + Math.random() * 900000);
-  //   // console.log(token);
-  //   const getD = await myUserModel.create({
-  //     name,
-  //     email,
-  //     password: hashed,
-  //     phoneNumber,
-  //     token,
-  //   });
-  //   sendEmail(getD);
-  //   return NextResponse.json({
-  //     message: "User Created",
-  //     status: 200,
-  //     data: getD,
-  //   });
-  // } catch (error: any) {
-  //   // eslint-disable-line @typescript-eslint/no-explicit-any
-  //   return NextResponse.json({
-  //     message: "Error Occured",
-  //     status: 400,
-  //     error: error.message,
-  //   });
-  // }
-};
+// export const POST = async (req: NextRequest, res: NextResponse) => {
+//   try {
+//     await dbConfig();
+//     const { name, email, password, phoneNumber } = await req.json();
+//     const salt = await bcrypt.genSalt(10);
+//     const hashed = await bcrypt.hash(password, salt);
+//     const token = Math.floor(100000 + Math.random() * 900000);
+
+//     // console.log(token);
+
+//     const getD = await myUserModel.create({
+//       name,
+//       email,
+//       password: hashed,
+//       phoneNumber,
+//       token,
+//     });
+//     sendEmail(getD);
+//     return NextResponse.json({
+//       message: "User Created",
+//       status: 200,
+//       data: getD,
+//     });
+//   } catch (error: any) {
+//     // eslint-disable-line @typescript-eslint/no-explicit-any
+//     return NextResponse.json({
+//       message: "Error Occured",
+//       status: 400,
+//       error: error.message,
+//     });
+//   }
+// };
 
 export const GET = async () => {
   try {
