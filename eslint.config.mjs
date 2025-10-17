955aaf77f093104b45e8c5c7ce60da3e6c7a718a
mac <mac@macs-MacBook-Pro.local>
2025-10-18 00:51:17 +0100

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,8 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
     },
   },
 ];
