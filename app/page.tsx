c00c250de84b42260e4a6b512c30d3919a2c0eb2
mac <mac@macs-MacBook-Pro.local>
2025-10-18 01:52:07 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,32 @@
-import React from 'react'
-import Hero from './comp/hero'
-import About from './comp/about'
-import Faq from './comp/faq'
-import Testimony from './comp/testimony'
-import Explore from './comp/explore'
-import Footer from './comp/footer'
-import Header from './comp/header'
+import React from "react";
+import Hero from "./comp/hero";
+import About from "./comp/about";
+import Faq from "./comp/faq";
+import Testimony from "./comp/testimony";
+import Explore from "./comp/explore";
+import Footer from "./comp/footer";
+import Header from "./comp/header";
 
 const Page = () => {
-
   return (
-
-    <div className={`bg-[url("../app/comp/imgs/bg1.png")] bg-center text-white`} >
+    <div
+      className={`bg-[url("../app/comp/imgs/bg1.png")] bg-center text-white`}
+    >
       <Header />
       {/* <Image className='h-[100%] absolute w-full object-cover' src={img} alt='#' /> */}
-      <div className='px-5 relative top-[20px] md:px-10'>
-         <Hero  />
-         
-          <Explore />
-       
-         
-      <About />
-      <Testimony />
-      <Faq />
-       <hr className="text-neutral-500" />
+      <div className="px-5 relative top-[20px] md:px-10">
+        <Hero />
+
+        <Explore />
+
+        <About />
+        <Testimony />
+        <Faq />
+        <hr className="text-neutral-500" />
         <Footer />
       </div>
-     
     </div>
-  )
-}
+  );
+};
 
-export default 
-Page
+export default Page;
