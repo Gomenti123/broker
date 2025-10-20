ee134ceb77adc3c713a2333eed4fd3cb807908ca
mac <mac@macs-MacBook-Pro.local>
2025-10-20 14:08:02 +0100

    update

--- app/api/verify/[userID]/route.ts
+++ app/api/verify/[userID]/route.ts
@@ -1,58 +1,58 @@
-// import { dbConfig } from "@/app/utils/dbConfig";
-// import myUserModel from "@/app/utils/Model/userModel";
-// import { Request } from "express";
-// import { NextRequest, NextResponse } from "next/server";
-// interface iParams {
-//   params: { slug: string; userID: string };
-// }
-// export const POST = async (req: NextRequest, { params }: iParams) => {
-//   try {
-//     await dbConfig();
-//     const { userID } = await params;
-//     const token = await req.json();
-//     // console.log(userID);
-//     // console.log(token);
+import { dbConfig } from "@/app/utils/dbConfig";
+import myUserModel from "@/app/utils/Model/userModel";
+import { Request } from "express";
+import { NextRequest, NextResponse } from "next/server";
+interface iParams {
+  params: { slug: string; userID: string };
+}
+export const POST = async (req: NextRequest, { params }: iParams) => {
+  try {
+    await dbConfig();
+    const { userID } = await params;
+    const token = await req.json();
+    // console.log(userID);
+    // console.log(token);
 
-//     const user = await myUserModel.findById(userID);
-//     // console.log(getD.token);
+    const user = await myUserModel.findById(userID);
+    // console.log(getD.token);
 
-//     if (user) {
-//       const checkIfVerified = user.verify === true;
-//       if (!checkIfVerified) {
-//         const tokenCheck = token.token === user.token;
-//         if (tokenCheck) {
-//           const getD = await myUserModel.findByIdAndUpdate(
-//             userID,
-//             { verify: true, token: "" },
-//             { new: true }
-//           );
-//           return NextResponse.json({
-//             message: "User Verified",
-//             status: 200,
-//             data: getD,
-//           });
-//         } else {
-//           return NextResponse.json({
-//             message: "Incorrect token",
-//             status: 400,
-//           });
-//         }
-//       } else {
-//         return NextResponse.json({
-//           message: "Account already verified",
-//           status: 400,
-//         });
-//       }
-//     } else {
-//       return NextResponse.json({
-//         message: "User not found",
-//         status: 400,
-//       });
-//     }
-//   } catch (error) {
-//     return NextResponse.json({
-//       message: "Error Occured",
-//       status: 400,
-//     });
-//   }
-// };
+    if (user) {
+      const checkIfVerified = user.verify === true;
+      if (!checkIfVerified) {
+        const tokenCheck = token.token === user.token;
+        if (tokenCheck) {
+          const getD = await myUserModel.findByIdAndUpdate(
+            userID,
+            { verify: true, token: "" },
+            { new: true }
+          );
+          return NextResponse.json({
+            message: "User Verified",
+            status: 200,
+            data: getD,
+          });
+        } else {
+          return NextResponse.json({
+            message: "Incorrect token",
+            status: 400,
+          });
+        }
+      } else {
+        return NextResponse.json({
+          message: "Account already verified",
+          status: 400,
+        });
+      }
+    } else {
+      return NextResponse.json({
+        message: "User not found",
+        status: 400,
+      });
+    }
+  } catch (error) {
+    return NextResponse.json({
+      message: "Error Occured",
+      status: 400,
+    });
+  }
+};
