3d386483a055f79653728852b554c7892606ec4e
mac <mac@macs-MacBook-Pro.local>
2025-10-21 13:09:12 +0100

    update

--- app/utils/email.ts
+++ app/utils/email.ts
@@ -21,40 +21,44 @@ export const sendEmail = async (user: any) => {
     subject: "Verify Your Email", // Replace with your desired subject
     text: "This is a plain text email body.", // Plain text content
     // or
-    html: ` <body style="padding: 0; margin: 0">
-    <div
-      style="width: 100%; height: min-h-200px; padding: 10px; background-color: gold"
-    >
-      <div style="display: flex; justify-content: center">
-        <h1>SFOX</h1>
-      </div>
+    html: "<h1>Hello</h1>",
 
-      <div style="display: flex; flex-direction: column; gap: 10px">
-        <p>Hello dear,</p>
-        <p>Thank you for registering with us,</p>
-      </div>
-      <p>Your verification code is ${user.token}</p>
-      <p>
-        This is a secure platform made for everyone who want to grow their
-        income. We stands to be the best platform so far. Kindly click on the
-        button below to verify your email
-      </p>
-      <button
-        style="
-          padding: 5px;
-          width: 200px;
-          font-size: 20px;
-          color: blue;
-          border: none;
-          border-radius: 10px;
-        "
-      >
-        <a href="https://broker-eight-theta.vercel.app/verify/${user._id}">Verify</a>
-        
-      </button>
-    </div>
-  </body>`, // HTML content (optional)
+    //   html: ` <body style="padding: 0; margin: 0">
+    //   <div
+    //     style="width: 100%; height: min-h-200px; padding: 10px; background-color: gold"
+    //   >
+    //     <div style="display: flex; justify-content: center">
+    //       <h1>SFOX</h1>
+    //     </div>
+
+    //     <div style="display: flex; flex-direction: column; gap: 10px">
+    //       <p>Hello dear,</p>
+    //       <p>Thank you for registering with us,</p>
+    //     </div>
+    //     <p>Your verification code is ${user.token}</p>
+    //     <p>
+    //       This is a secure platform made for everyone who want to grow their
+    //       income. We stands to be the best platform so far. Kindly click on the
+    //       button below to verify your email
+    //     </p>
+    //     <button
+    //       style="
+    //         padding: 5px;
+    //         width: 200px;
+    //         font-size: 20px;
+    //         color: blue;
+    //         border: none;
+    //         border-radius: 10px;
+    //       "
+    //     >
+    //       <a href="https://broker-eight-theta.vercel.app/verify/${user._id}">Verify</a>
+
+    //     </button>
+    //   </div>
+    // </body>`,
   };
+
+  console.log("Just passed mail options");
 
   transporter.sendMail(mailOptions, (error) => {
     console.log("e reach transporter");
